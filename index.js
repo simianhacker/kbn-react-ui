21f4d47c94e3f141640b840cf4758ee0ee8285af
Chris Cowan <chris@chriscowan.us>
2016-11-10 11:17:13 -0700

    Adding the entry point for the module

---
+++ index.js
@@ -0,0 +1,11 @@
+import Header from './src/header/header';
+import Timepicker from './src/timepicker/picker';
+import TimepickerConfig from './src/timepicker/config';
+import IntervalConfig from './src/timepicker/interval_config';
+
+export default {
+  Header,
+  Timepicker,
+  TimepickerConfig,
+  IntervalConfig
+};
